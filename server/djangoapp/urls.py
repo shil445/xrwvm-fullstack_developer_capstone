5335f805954da95e95e21bf9f7f240348060a009
shil445 <shilpag@skillup.online>
2025-07-30 08:34:51 -0400

    Added new super user and implemented login function

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,16 +1,21 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
+from django.contrib.auth.views import LoginView
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+    path(route='login', view=views.login_user, name='login'),
+    
+    # path for logout
+    #path(route='logout', view=views.logout_view, name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    
     # path for dealer reviews view
 
     # path for add a review view
